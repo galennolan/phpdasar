3b0765d1a8bcbd52b47f923d4a2c7fb2feb3a769
galennolan <galen.nolan1@gmail.com>
2022-07-07 15:29:36 +0700

    lagi

---
+++ 12_Perbandingan.php
@@ -0,0 +1,13 @@
+<?php
+ /*Operator Perbandingan 
+ untuk membandingkan 2 value dengan hasilnya boolean
+ $a==$b sama dengan sama 
+ $a==$b sama plus tipe datanya sama
+ $a!=$b / $a<>$b Tidak sama dengan 
+ $a!==$b Tidak sama dengan plus tipe data juga beda
+  
+ */
+
+ var_dump(8 == "8");
+ var_dump(8 ==="8");
+?>
---
+++ 13_Logika.php
@@ -0,0 +1,15 @@
+<?php
+ /*Operator Logika 
+ $a && $b / $a and $b True
+ $a || $b / $a or $b True jika salah satu true
+ ! $a true jika $a bernilai false
+ $a xor $b true jika $a dan $b salah satu true, tapi tidak keduanya
+
+ 
+ */
+
+ var_dump(true && true);
+ var_dump(false || true);
+ var_dump(true xor true);
+ var_dump(!true);
+?>
